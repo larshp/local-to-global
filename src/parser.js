c1bfe39d76de3e180e0a34b49bcf73c01136eb25
larshp <larshp@hotmail.com>
2017-06-07 18:57:11 +0000

    more

--- src/parser.js
+++ src/parser.js
@@ -12,14 +12,18 @@ module.exports = class Parser {
     for (let line of lines) {
       line = line.replace(/lcl_/g, "zcl_abapgit_");
       line = line.replace(/lif_/g, "zif_abapgit_");
+      line = line.replace(/lcx_/g, "zcx_abapgit_");
       line = line.replace(/LCL_/g, "ZCL_ABAPGIT_");
       line = line.replace(/LIF_/g, "ZIF_ABAPGIT_");
+      line = line.replace(/LCX_/g, "ZCX_ABAPGIT_");
 
       if (start === ""
-          && line.match(/^\s*CLASS \w.*/i)
+          && ( line.match(/^\s*CLASS \w.*/i)
+          || line.match(/^\s*INTERFACE \w.*/i))
           && !line.includes("DEFERRED")) {
         start = line;
-      } else if (line.match(/^\s*ENDCLASS\..*/i)) {
+      } else if (line.match(/^\s*ENDCLASS\..*/i)
+          || line.match(/^\s*ENDINTERFACE\..*/i)) {
         body.push(line);
         this.classify(start, body.join("\n"));
         start = "";
@@ -40,7 +44,12 @@ module.exports = class Parser {
       return;
     }
 
-    let name = start.match(/^\s*CLASS\s(\w+).*/i)[1];
+    let name = "";
+    if (start.includes("CLASS")) {
+      name = start.match(/^\s*CLASS\s(\w+).*/i)[1];
+    } else {
+      name = start.match(/^\s*INTERFACE\s(\w+).*/i)[1];
+    }
 
     if(!this.list[name]) {
       this.list[name] = {};
@@ -50,6 +59,8 @@ module.exports = class Parser {
       this.list[name].def = body;
     } else if(start.includes("IMPLEMENTATION")) {
       this.list[name].impl = body;
+    } else {
+      this.list[name].interface = body;
     }
     if(start.includes("FOR TESTING")) {
       this.list[name].testing = true;
--- src/writer.js
+++ src/writer.js
@@ -4,12 +4,65 @@ var fs = require('fs');
 module.exports = class Writer {
   run(outDir, classes) {
     for(let cla in classes) {
+      let contents = "";
+      let meta = "";
+
       if (classes[cla].testing) {
         continue;
+      } else if(classes[cla].interface) {
+        contents = classes[cla].interface;
+        meta = this.interfaceMeta(cla);
+      } else {
+        contents = classes[cla].def + "\n\n" + classes[cla].impl;
+        meta = this.classMeta(cla);
       }
-      fs.writeFileSync(outDir + cla + ".abap",
-        classes[cla].def + "\n\n" + classes[cla].impl,
-        "UTF8");
+
+      if (cla.length > 30) {
+        console.log("rename: " + cla);
+      }
+
+      fs.writeFileSync(outDir + cla + ".abap", contents, "UTF8");
+      fs.writeFileSync(outDir + cla + ".xml", meta, "UTF8");
     }
+  }
+
+  interfaceMeta(name) {
+    return "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+      "<abapGit version=\"v1.0.0\" serializer=\"LCL_OBJECT_INTF\" serializer_version=\"v1.0.0\">\n" +
+      " <asx:abap xmlns:asx=\"http://www.sap.com/abapxml\" version=\"1.0\">\n" +
+      "  <asx:values>\n" +
+      "   <VSEOINTERF>\n" +
+      "    <CLSNAME>" + name.toUpperCase() + "</CLSNAME>\n" +
+      "    <VERSION>1</VERSION>\n" +
+      "    <LANGU>E</LANGU>\n" +
+      "    <DESCRIPT></DESCRIPT>\n" +
+      "    <EXPOSURE>2</EXPOSURE>\n" +
+      "    <STATE>1</STATE>\n" +
+      "    <UNICODE>X</UNICODE>\n" +
+      "   </VSEOINTERF>\n" +
+      "  </asx:values>\n" +
+      " </asx:abap>\n" +
+      "</abapGit>";
+  }
+
+  classMeta(name) {
+    return "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+      "<abapGit version=\"v1.0.0\" serializer=\"LCL_OBJECT_CLAS\" serializer_version=\"v1.0.0\">\n" +
+      " <asx:abap xmlns:asx=\"http://www.sap.com/abapxml\" version=\"1.0\">\n" +
+      "  <asx:values>\n" +
+      "   <VSEOCLASS>\n" +
+      "    <CLSNAME>" + name.toUpperCase() + "</CLSNAME>\n" +
+      "    <VERSION>1</VERSION>\n" +
+      "    <LANGU>E</LANGU>\n" +
+      "    <DESCRIPT></DESCRIPT>\n" +
+      "    <EXPOSURE>2</EXPOSURE>\n" +
+      "    <STATE>1</STATE>\n" +
+      "    <CLSCCINCL>X</CLSCCINCL>\n" +
+      "    <FIXPT>X</FIXPT>\n" +
+      "    <UNICODE>X</UNICODE>\n" +
+      "   </VSEOCLASS>\n" +
+      "  </asx:values>\n" +
+      " </asx:abap>\n" +
+      "</abapGit>";
   }
 }
